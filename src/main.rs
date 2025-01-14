099923258a373fc08cdb419373338e460fd275d6
realcathode <ion.miron@tutanota.com>
2025-01-14 20:53:01 +0200

    Implement REPL

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-
-    println!("{}: command not found", input.trim());
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+    
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+    
+        println!("{}: command not found", input.trim());    
+    }
 }
