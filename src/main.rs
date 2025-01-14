bc6bf0909b29f8d5af7280bfdda2200a31e7b2b3
realcathode <ion.miron@tutanota.com>
2025-01-14 22:06:26 +0200

    Add type function Add functions for echo and exit

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,59 @@
 use std::io::{self, Write};
 use std::process;
 
+const BUILTIN: [&str; 3] = ["echo", "exit", "type"];
+
+fn sh_exit(args: Vec<&str>) {
+    match args.get(1) {
+        Some(&v) => match v.parse::<i32>() {
+            Ok(code) => {
+                process::exit(code);
+            },
+            Err(_) => {
+                process::exit(1);
+            }
+        },
+        None => process::exit(0),
+    }
+}
+
+fn sh_echo(args: Vec<&str>) {
+    for i in 1..args.len() {
+        match args.get(i) {
+            Some(&s) => {
+                print!("{}", s);
+            },
+            None => {
+                println!("args[{}] out of bounds", i);
+            }
+        }
+        if i != args.len() - 1 {
+            print!(" ");
+        }
+    }
+    println!("");
+}
+
+fn sh_type(args: Vec<&str>) {
+    for i in 1..args.len() {
+        match args.get(i) {
+            Some(&s) => {
+                if BUILTIN.contains(&s) {
+                    print!("{} is a shell builtin", s);
+                }else {
+                    print!("{}: not found", s);
+                }
+            },
+            None => {
+            },
+        }
+        if i != args.len() - 1 {
+            print!(" ");
+        }
+    }
+    println!("");
+}
+
 fn main() {
     let stdin = io::stdin();
 
@@ -11,37 +64,23 @@ fn main() {
         io::stdout().flush().unwrap();
     
         stdin.read_line(&mut input).unwrap();
+        if input.trim().is_empty() {
+            continue;
+        }
+        
         let args: Vec<&str> = input.trim().split_ascii_whitespace().collect();
+
+        
         match args[0].trim() {
             "exit" => {
-                match args.get(1) {
-                    Some(&v) => match v.parse::<i32>() {
-                        Ok(code) => {
-                            // println!("Exiting with code {}", code);
-                            process::exit(code);
-                        },
-                        Err(_) => {
-                            // println!("Invalid exit code");
-                            continue;
-                        }
-                    },
-                    None => process::exit(0),
-                }
+                sh_exit(args);
             },
             "echo" => {
-                for i in 1..args.len() {
-                    match args.get(i) {
-                        Some(&s) => {
-                            print!("{}", s);
-                        },
-                        None => {
-                            println!("args[{}] out of bounds", i);
-                        }
-                    }
-                    print!(" ");
-                }
-                println!("");
+                sh_echo(args);
             },
+            "type" => {
+                sh_type(args);
+            }
             _ => {
                 println!("{}: command not found", input.trim());    
             }
