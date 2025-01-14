e5887c23cf6c97615704f613a330171734ecb14e
realcathode <ion.miron@tutanota.com>
2025-01-14 21:05:53 +0200

    Handle non-existent commands Add basic exit command

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,18 @@ fn main() {
     let stdin = io::stdin();
 
     loop {
+        let mut input = String::new();
         print!("$ ");
         io::stdout().flush().unwrap();
     
-        let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
     
-        println!("{}: command not found", input.trim());    
+        match input.trim() {
+            "exit 0" => break,
+            _ => {
+                println!("{}: command not found", input.trim());    
+            }
+        }
+        io::stdout().flush().unwrap();
     }
 }
